b9b603a2fe2bf84a62e131407d261a4772d870b9
aslukyanov <andrey.lukianov89@gmail.com>
2022-06-16 13:50:23 +0300

    change task_3

--- hw7/task_3.py
+++ hw7/task_3.py
@@ -1,4 +1,4 @@
-
+import os
 
 def create_dict_for_write(files : list, folder : str) -> dict :
     wr_dict = {}
@@ -9,19 +9,35 @@ def create_dict_for_write(files : list, folder : str) -> dict :
     return wr_dict
 
 
+def check_file_exist(folder, new_file_name) :
+    is_file_exists = os.path.exists(folder + new_file_name)
+    if is_file_exists :
+        print("Warning!! This file is already exist. Continue will erase this file")
+        answer = input("Do you want to continue (Yes/No): ")
+        if answer == "Yes" :
+            os.remove(folder + new_file_name)
+            print(f"File {folder + new_file_name} has been deleted")
+            return True
+        else :
+            return False
+    else :
+        return True
+    
+
 def create_new_file(files : list, folder : str, new_file_name : str) :
     """Create new file contains files from list"""
-    wr_dict = create_dict_for_write(files, folder)
-    with open(folder + new_file_name, "a") as result :
-        for lenght in sorted(list(wr_dict.keys())) :
-            result.write(wr_dict[lenght][0] + "\n")
-            result.write(str(len(wr_dict[lenght][1])) + "\n")
-            for line in wr_dict[lenght][1] :
-                if line == wr_dict[lenght][1][-1] :
-                    result.write(line + "\n")
-                else :
-                    result.write(line)
-    print(f"File {new_file_name} has been created in folder {folder}")
+    if check_file_exist(folder, new_file_name) :
+        wr_dict = create_dict_for_write(files, folder)
+        with open(folder + new_file_name, "a") as result :
+            for lenght in sorted(list(wr_dict.keys())) :
+                result.write(wr_dict[lenght][0] + "\n")
+                result.write(str(len(wr_dict[lenght][1])) + "\n")
+                for line in wr_dict[lenght][1] :
+                    if line == wr_dict[lenght][1][-1] :
+                        result.write(line + "\n")
+                    else :
+                        result.write(line)
+        print(f"File {new_file_name} has been created in folder {folder}")
 
 
 
@@ -29,7 +45,7 @@ if __name__ == "__main__" :
     files = ['1.txt', '2.txt', '3.txt']
     folder = "/home/andrey/git/netology/hw7/"
     create_new_file(files, folder, "result.txt")
-
+    # check_file_exist(folder, "result.txt")
 
 
 
